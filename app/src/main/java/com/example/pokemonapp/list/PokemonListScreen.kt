1461f16d8d98597562623ff9f7f086bfc456f8cd
Amresh <iamresh8@gmail.com>
2021-07-25 19:17:39 +0200

    removed unused imports

--- app/src/main/java/com/example/pokemonapp/list/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokemonapp/list/PokemonListScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -29,7 +28,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 import coil.request.ImageRequest
 import com.example.pokemonapp.R
 import com.example.pokemonapp.data.model.PokedexListEntry
